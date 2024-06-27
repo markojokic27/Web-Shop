7403a3c5a8f57677cb0e746415bec20046e8820a
markojokic27 <jokicmarko98@gmail.com>
2024-06-27 11:18:33 +0200

    json file edited

--- medusa/src/pages/Shop/Shop.jsx
+++ medusa/src/pages/Shop/Shop.jsx
@@ -1,16 +1,20 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import classes from "./index.module.css";
 import { FormContext } from "../../context/formContext";
 import ProductCard from "./ProductCard/ProductCard";
 function Shop() {
   const { products } = useContext(FormContext);
-  console.log(products);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  useEffect(() => {
+    setFilteredProducts(products);
+  }, [products]);
   return (
     <div className={classes.shop}>
       <div className={classes.shop__wrapper}>
         <h1>Shop</h1>
+        
         <div className={classes.shop__products}>
-          {products.map((product) => (
+          {filteredProducts.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
