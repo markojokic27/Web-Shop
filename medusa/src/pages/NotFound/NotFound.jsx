66b7b3a4fceac297b1dfc5537975c54461b40788
markojokic27 <jokicmarko98@gmail.com>
2024-06-27 17:40:41 +0200

    not found page done

--- medusa/src/pages/NotFound/NotFound.jsx
+++ medusa/src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import classes from "./index.module.css";
+import { useEffect } from "react";
 
 function NotFound() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate(-1);}, 3000);
+  }, [navigate]);
   return (
     <div className={classes.notFound}>
-      <h1>404</h1>
+      <h1>NOT FOUND</h1>
+      <p>Returning to the previous page in 3 seconds.</p>
     </div>
   );
 }
